8d0e8f1c09487fe76c1aeaaa4cfca508ffc20553
Drew Short <warrick@sothr.com>
2015-10-09 21:08:15 -0500

    Working on Dropbox integration

--- rustbuild-linker.go
+++ rustbuild-linker.go
@@ -6,18 +6,22 @@ import (
 	"log"
 	//"net/http"
 	"gopkg.in/yaml.v2"
-	//"github.com/stacktic/dropbox"
+	"github.com/stacktic/dropbox"
 	//"github.com/ant0ine/go-json-rest/rest"
 )
 
 type Config struct {
-	api_secret string
-	api_key string
-	app_folder string
-	socket_file string
+	Api_secret string
+	Api_key string
+	Client_token string
+	App_folder string
+	Socket_file string
 }
 
 func main() {
+	var err error
+    var db *dropbox.Dropbox
+
 	// The struct to store the configuration data
 	config := Config{}
 
@@ -30,5 +34,26 @@ func main() {
 	if yaml_err != nil {
     	log.Fatal("error: %v", yaml_err)
     }
-    fmt.Printf("--- config dump:\n%s\n\n", string(data))
+    fmt.Printf("--- config_file dump:\n%s\n\n", data)
+    fmt.Printf("--- config dump:\n%s\n\n", config)
+
+	db = dropbox.NewDropbox()
+	db.SetAppInfo(config.Api_key, config.Api_secret)
+	if len(config.Client_token) >= 1 {
+		db.SetAccessToken(config.Client_token)
+	} else {
+		if err = db.Auth(); err != nil {
+	        fmt.Println(err)
+	        return
+	    }
+	    config.Client_token = db.AccessToken()
+	    db.SetAccessToken(config.Client_token)
+	    d, marshal_err := yaml.Marshal(&config)
+        if marshal_err != nil {
+            log.Fatal("error: %v", marshal_err)
+        }
+        ioutil.WriteFile("config.yml",[]byte(d), 0644)
+	}
+
+	
 }
