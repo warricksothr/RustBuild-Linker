13582c55023e2f15a8123579140ed049b58a564c
Drew Short <warrick@sothr.com>
2015-10-22 15:54:15 -0500

    Made a build directory. Tweaked the build script. Updated the gitignore

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ go clean
 go fmt
 go get || true
 go build -o "rbl_$(git rev-parse --short HEAD)_$(date +%Y-%m-%d)" -p $(($(nproc)-1)) -ldflags "-s" rustbuild-linker.go
+
+mkdir -p builds
+mv rbl_* builds
