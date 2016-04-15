1a31e1dfbf7cc0b02da183ac7dcb08eecb30a87f
Drew Short <warrick@sothr.com>
2016-04-15 17:22:54 -0500

    Working on deployment script
    Added a deployment script to simplify newer versions going to production.
    Updated the build script to support the deploy script.

--- build.sh
+++ build.sh
@@ -12,3 +12,5 @@ go build -o "rbl_$(date +%Y-%m-%d)_$(git rev-parse --short HEAD)" -p $(($(nproc)
 
 mkdir -p builds
 mv rbl_* builds
+LATEST_BUILD=$(ls -t builds/rbl_* | head -1)
+ln -sf LATEST_BUILD latest_build
---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+# Generic deployment script
+
+DEPLOY_CONFIG_FILE="./deploy_config.sh"
+
+set -x
+set -e
+
+if [ -f $DEPLOY_CONFIG_FILE ]; then
+    echo "Loading Config File [$DEPLOY_CONFIG_FILE]"
+    source $DEPLOY_CONFIG_FILE
+else
+    echo "Deployment Config File [$DEPLOY_CONFIG_FILE] Missing. Can't Continue."
+    exit -1
+fi
+
+LATEST_BUILD=$(ls -t builds/rbl_* | head -1)
+scp -i $DEPLOY_USER_CERT -P $DEPLOY_PORT $LATEST_BUILD ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_BUILD_TARGET}/
+ssh -i $DEPLOY_USER_CERT -p $DEPLOY_PORT ${DEPLOY_USER}@${DEPLOY_HOST} "ln -sf '${DEPLOY_BUILD_TARGET}/${LATEST_BUILD}' ${DEPLOY_LINK_TARGET}"
